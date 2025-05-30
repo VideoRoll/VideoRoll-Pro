ce165f16acb9bfdc17baa717c8f4ff46985f26c5
gomi <gxy880520@qq.com>
2025-05-30 09:38:26 +0800

    chore: 移除ffmpeg库

--- src/download/hooks/useFFmpeg.ts
+++
@@ -1,9 +0,0 @@
-import { FFmpeg } from '@ffmpeg/ffmpeg';
-import { markRaw } from "vue";
-
-export function useFFmpeg() {
-    const ffmpeg = markRaw(new FFmpeg());
-    return ffmpeg;
-}
-
-export * from '@ffmpeg/util';
